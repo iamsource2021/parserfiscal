254d1ee6db50a0e9ea221358592f7c36154e3544
Eduardo Ortiz <eduardooa1980@gmail.com>
2022-10-25 15:36:10 -0500

    agregue navbar

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {AmplifyProvider} from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsconfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>  
 );
 
 // If you want to start measuring performance in your app, pass a function
