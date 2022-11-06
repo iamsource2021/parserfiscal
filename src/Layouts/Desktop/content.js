7ef325677070dc19c50b9cc9b9d2c081b252e35a
Eduardo Ortiz <eduardooa1980@gmail.com>
2022-11-06 10:40:01 -0600

    add scrollbutton

--- src/Layouts/Desktop/content.js
+++ src/Layouts/Desktop/content.js
@@ -1,6 +1,6 @@
 import Outlet from "../../Pages/Outlet/index.js";
 
-function Content(props) {
+function Content(props) {    
     return (
         <Outlet />
     );
--- src/Layouts/Desktop/index.js
+++ src/Layouts/Desktop/index.js
@@ -3,13 +3,18 @@ import Content from './content.js';
 import Footerv1 from './../../Components/Footerv1/index';
 import propsData  from './../../Context/Footerv1/index';
 
+import { Fragment } from 'react';
+import ScrollButton from './../../Components/ScrollButton/index';
+
 function Desktop(props) {
+    document.title = 'e - Despliegue aplicaciones nativas en la nube de forma facil.'
     return (
-        <>
+        <Fragment>
             <Header />
             <Content />
             <Footerv1 className="footer-instance-1" {...propsData} />
-        </>
+            <ScrollButton />
+        </Fragment>
     );
 }
 
